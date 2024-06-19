210c925f79c4ee86390fa84d317bd33b25748311
JuanB-18 <jua.broncano@duocuc.cl>
2024-06-19 16:00:58 -0400

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -12,4 +12,20 @@ def multiplicar(num1,num2):
 def dividir(num1,num2):
     resultado= num1/num2
     return resultado
-numero1=int(input("Ingrese un numero:\n"))
+while True:
+    try:
+        operacion=int(input("¿Que operacion desea realizar?\n1.-Sumar\n2.-Restar\n3.-Multiplicar\n4.-Dividir\n5.-Salir\n"))
+    except ValueError as e:
+        print("Seleccione correctamente")
+        continue
+    numero1=int(input("Ingrese un numero:\n"))
+    numero2=int(input("Ingrese otro numero:\n"))
+    if operacion == 1:
+        print(sumar(numero1,numero2))
+    elif operacion==2:
+        print(restar(numero1,numero2))
+    elif operacion==3:
+        print(multiplicar(numero1,numero2))
+    elif operacion == 4:
+        print(dividir(numero1,numero2))
+    print("chao")
