79ddf46bdeb9909c6f32f8dffb925360bd59b074
JuanB-18 <jua.broncano@duocuc.cl>
2024-06-19 15:30:12 -0400

    Update calculadora.py
    Co-Authored-By: AleDuoc1010 <173198674+AleDuoc1010@users.noreply.github.com>

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,14 @@
 #este es el archivo python donde trabajaremos de manera colaborativa
 #hola
+def sumar (num1,num2):
+    resultado= num1+num2
+    return resultado
+def restar(num1,num2):
+    resultado= num1-num2
+    return resultado
+def multiplicar(num1,num2):
+    resultado= num1*num2
+    return resultado
+def dividir(num1,num2):
+    resultado= num1/num2
+    return resultado
